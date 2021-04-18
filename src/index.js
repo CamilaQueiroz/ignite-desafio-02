331d7fe34de7eeabe649e878859f806fee509032
Camila Queiroz <camilaqueiroz@mbp-de-camila.home>
2021-04-18 14:40:15 -0300

    Desafio 02 concluido

--- src/index.js
+++ src/index.js
@@ -10,19 +10,56 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Username doesnot exists' });
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length === 10) {
+    return response.status(403).json({ error: 'To create more than 10 todos you need to subscribe to the pro plan' });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  
+  const user = users.find(user => user.username === username);
+  if (!user) return response.status(404).json({ error: 'Username doesnot exists' });
+
+  if (!validate(id)) return response.status(400).json({ error: 'It is not a valid uuid' });
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) return response.status(404).json({ error: 'This todo doesnot exists' });
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
